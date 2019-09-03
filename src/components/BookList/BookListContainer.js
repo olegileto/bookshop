a34d84d9dc73d4d3e3d9a3cbe6bd925104f93621
Oleg Khmelevskoy <olegileto@gmail.com>
2019-09-03 15:04:18 +0300

    Refactoring #3 Reducers

--- src/components/BookList/BookListContainer.js
+++ src/components/BookList/BookListContainer.js
@@ -33,7 +33,7 @@ class BookListContainer extends Component {
 }
 
 
-const mapStateToProps = ({books, loading, error}) => {
+const mapStateToProps = ({bookList: {books, loading, error}}) => {
     return {books, loading, error}
 };
 
